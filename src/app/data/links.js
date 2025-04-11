6e68e09f07e6ef89ea8aa468408df83e30793686
jonatan <95307849+truthfulle@users.noreply.github.com>
2025-04-11 20:40:46 +0100

    Update links.js

--- src/app/data/links.js
+++ src/app/data/links.js
@@ -1,12 +1,9 @@
 export const socialLinks = [
-  { name: 'GitHub', url: 'https://github.com', icon: 'github', color: '#181717', display: true },
-  { name: 'LinkedIn', url: 'https://linkedin.com', icon: 'linkedin', color: '#0A66C2', display: true },
-  { name: 'Twitter', url: 'https://twitter.com', icon: 'twitter', color: '#1DA1F2', display: false },
-  { name: 'Instagram', url: 'https://instagram.com', icon: 'instagram', color: '#E4405F', display: false },
-  { name: 'YouTube', url: 'https://youtube.com', icon: 'youtube', color: '#FF0000', display: false },
-  { name: 'Discord', url: 'https://discord.com', icon: 'discord', color: '#5865F2', display: false },
+  { name: 'GitHub', url: 'https://github.com/truthfulle', icon: 'github', color: '#181717', display: true },
+  { name: 'LinkedIn', url: 'https://linkedin.com/jonatanwiankowski', icon: 'linkedin', color: '#0A66C2', display: true },
+  { name: 'Twitter', url: 'https://twitter.com/truthfulle1', icon: 'twitter', color: '#1DA1F2', display: false },
+  { name: 'Instagram', url: 'https://instagram.com/akajonatan', icon: 'instagram', color: '#E4405F', display: false },
   { name: 'Spotify', url: 'https://spotify.com', icon: 'spotify', color: '#1DB954', display: false },
   { name: 'Reddit', url: 'https://reddit.com', icon: 'reddit', color: '#FF4500', display: false },
-  { name: 'Email', url: 'mailto:example@example.com', icon: 'gmail', color: '#EA4335', display: true },
-  { name: 'Website', url: 'https://yourwebsite.com', icon: 'globe', color: '#4285F4', display: false },
+  { name: 'Email', url: 'mailto:jonatan.wiankowski@gmail.com', icon: 'gmail', color: '#EA4335', display: true },
 ];
