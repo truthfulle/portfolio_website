ef07915cedc03e14b91f393ccbe2a8566209b927
jonatan <95307849+truthfulle@users.noreply.github.com>
2025-04-11 20:50:32 +0100

    Update links.js

--- src/app/data/links.js
+++ src/app/data/links.js
@@ -1,6 +1,6 @@
 export const socialLinks = [
   { name: 'GitHub', url: 'https://github.com/truthfulle', icon: 'github', color: '#181717', display: true },
-  { name: 'LinkedIn', url: 'https://linkedin.com/jonatanwiankowski', icon: 'linkedin', color: '#0A66C2', display: true },
+  { name: 'LinkedIn', url: 'https://www.linkedin.com/in/jonatanwiankowski/', icon: 'linkedin', color: '#0A66C2', display: true },
   { name: 'Twitter', url: 'https://twitter.com/truthfulle1', icon: 'twitter', color: '#1DA1F2', display: false },
   { name: 'Instagram', url: 'https://instagram.com/akajonatan', icon: 'instagram', color: '#E4405F', display: false },
   { name: 'Spotify', url: 'https://open.spotify.com/user/mg8talroom1qazll7ch8tnu6f?si=4gYOFD6CR6CsPrENKMqVIg&nd=1&dlsi=90287df5ff344777', icon: 'spotify', color: '#1DB954', display: false },
